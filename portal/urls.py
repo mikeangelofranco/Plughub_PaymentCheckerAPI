c7546b19c0577019af73b70fe9fbcc56c054d19e
Mike <mikeangelofranco@gmail.com>
2025-11-22 12:22:56 +0800

    deployment changes

--- portal/urls.py
+++ portal/urls.py
@@ -6,7 +6,11 @@ app_name = "portal"
 
 urlpatterns = [
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("dashboard", DashboardView.as_view()),
     path("api/checkuserdetails/", check_user_details, name="checkuserdetails"),
+    path("api/checkuserdetails", check_user_details),
     path("api/logpayments/", log_payments, name="logpayments"),
+    path("api/logpayments", log_payments),  # allow no trailing slash (webhooks)
     path("api/licenseconsume/", license_consume, name="licenseconsume"),
+    path("api/licenseconsume", license_consume),
 ]
